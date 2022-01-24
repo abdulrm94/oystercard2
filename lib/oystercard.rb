b07f803d72baddfe4eea5c5af52e8dab35a33ef2
Daniel Watts <d.watts15@outlook.com>
2022-01-24 15:10:04 +0000

    Create balance for oystercard and created associated test.

---
+++ lib/oystercard.rb
@@ -0,0 +1,13 @@
+# In order to use public transport
+# As a customer
+# I want money on my card
+
+# Nouns: public transport, 
+
+class Oystercard
+  attr_reader (:balance = 0)
+
+  def balance
+  end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,10 @@
+require 'oystercard'
+
 describe Oystercard do
+
+  let(:oystercard) { Oystercard.new }
+  
+  it "checks for an existing balance" do
+    expect(oystercard).to respond_to(:balance)
+  end
 end
