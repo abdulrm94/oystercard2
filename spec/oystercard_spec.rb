cc355e144dfb28307d111271010cc28a99c1462a
Daniel Watts <d.watts15@outlook.com>
2022-01-24 15:54:27 +0000

    Tested for top_up to sum multiple given amounts.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -18,4 +18,12 @@ describe Oystercard do
     oystercard.top_up(50)
     expect(oystercard.balance).to eq(50)
   end
+
+  it "checks that top_up will sum given amounts to the balance" do
+    oystercard.top_up(50)
+    oystercard.top_up(20)
+    oystercard.top_up(30)
+    expect(oystercard.balance).to eq(100)
+  end
+
 end
